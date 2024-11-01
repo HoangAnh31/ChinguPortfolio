10a5dcf75360268e98c199ee505b0ec83e26ad19
Nguyen Pham Hoang Anh <anhnph.us@gmail.com>
2024-11-01 22:03:37 +1000

    update app js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/anhnguyen-portfolio/" element={<Home />} />
+        <Route path="/ChinguPortfolio/" element={<Home />} />
       </Routes>
     </>
   );
